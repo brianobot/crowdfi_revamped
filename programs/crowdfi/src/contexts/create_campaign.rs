4313b53ee9c3d816b32dfd8241727454f2b0a021
Brian Obot <brianobot@gmail.com>
2025-02-26 14:12:11 +0100

    Refactor campaign context and update test configurations; add Jest support and improve README with testing section

--- programs/crowdfi/src/contexts/create_campaign.rs
+++ programs/crowdfi/src/contexts/create_campaign.rs
@@ -1,5 +1,20 @@
 use anchor_lang::prelude::*;
-use anchor_spl::{metadata::mpl_token_metadata, token_interface::{Mint, TokenInterface}};
+use anchor_spl::{
+    metadata::{
+        mpl_token_metadata::{
+            self, instructions::{
+                CreateMetadataAccountV3Cpi, 
+                CreateMetadataAccountV3CpiAccounts, 
+                CreateMetadataAccountV3InstructionArgs
+            }, 
+            types::DataV2
+        }, MetadataAccount}, 
+        token_interface::{
+            Mint, 
+            TokenInterface
+        }
+    };
+use anchor_spl::metadata::Metadata;
 
 use crate::state::{Campaign, Config};
 use crate::error::CrowdfiError;
@@ -30,7 +45,7 @@ pub struct CreateCampaign<'info> {
         bump,
     )]
     // this would still be owned by my program
-    pub campaign_vault: SystemAccount<'info>,
+    pub vault: SystemAccount<'info>,
     #[account(
         init,
         payer = admin,
@@ -42,8 +57,19 @@ pub struct CreateCampaign<'info> {
         seeds = [b"reward_mint", campaign.key().as_ref()],
         bump,
     )]
-    pub campaign_reward_mint: InterfaceAccount<'info, Mint>,
+    pub reward_mint: InterfaceAccount<'info, Mint>,
+    // #[account(
+    //     seeds = [
+    //         b"metadata",
+    //         metadata_program.key().as_ref(),
+    //         campaign_reward_mint.key().as_ref(),
+    //     ],
+    //     seeds::program = metadata_program.key(),
+    //     bump,
+    // )]
+    // pub reward_mint_metadata: Account<'info, MetadataAccount>,
     pub system_program: Program<'info, System>,
+    // pub metadata_program: Program<'info, Metadata>,
     pub token_program: Interface<'info, TokenInterface>,
 }
 
@@ -64,23 +90,66 @@ impl<'info> CreateCampaign<'info> {
             start_timestamp,
             end_timestamp,
             bump: bumps.campaign,
-            vault_bump: bumps.campaign_vault, 
-            reward_mint_bump: bumps.campaign_reward_mint,
+            vault_bump: bumps.vault, 
+            reward_mint_bump: bumps.reward_mint,
             is_completed: false
         });
         Ok(())
     }
 
-    pub fn create_mint_metadata(&mut self) -> Result<()> {
-        let seeds = [
-            b"metadata",
-            self.campaign_reward_mint.to_account_info().key.as_ref(),
-            mpl_token_metadata::ID.as_ref(),
-        ];
+    pub fn init_mint_metadata(&mut self, title: String) -> Result<()> {
+        // let name = title;
+        // let symbol = String::from("NNN");
+        // let uri = String::from("https://brianobot.github.io/");
 
-        let (_metadata_key, _bump) = Pubkey::find_program_address(&seeds, &mpl_token_metadata::ID);
+        // let seeds = [
+        //     b"metadata",
+        //     self.reward_mint.to_account_info().key.as_ref(),
+        //     mpl_token_metadata::ID.as_ref(),
+        // ];
 
-        // mpl_token_metadata::accounts::CreateMetadataAccountsV3
+        // let signer_seeds = &[&seeds[..]];
+
+        // let cpi_program = self.token_program.to_account_info();
+
+        // // metadata,
+        // // mint,
+        // // mint_authority,
+        // // update_authority: (update_authority, true),
+        // // payer,
+        // // system_program,
+        // // rent: None,
+
+        // let cpi_accounts = CreateMetadataAccountV3CpiAccounts {
+        //     metadata: &self.reward_mint_metadata.to_account_info(),
+        //     mint: &self.reward_mint.to_account_info(),
+        //     mint_authority: &self.campaign.to_account_info(),
+        //     payer: &self.admin.to_account_info(),
+        //     update_authority: (&self.campaign.to_account_info(), false),
+        //     system_program: &self.system_program.to_account_info(),
+        //     rent: None,
+        // };
+
+        // let args = CreateMetadataAccountV3InstructionArgs {
+        //     data: DataV2 {
+        //         name,
+        //         symbol,
+        //         uri,
+        //         seller_fee_basis_points: 0,
+        //         creators: None,
+        //         collection: None,
+        //         uses: None,
+        //     },
+        //     is_mutable: true,
+        //     collection_details: None,
+        // };
+
+        // CreateMetadataAccountV3Cpi::new(
+        //     &cpi_program,
+        //     cpi_accounts,
+        //     args,
+        // )
+        // .invoke_signed(signer_seeds)?;
 
         Ok(())
     }
